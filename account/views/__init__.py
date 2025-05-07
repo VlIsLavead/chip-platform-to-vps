2df3d1e082e5fe2cfbcf1d35f401679638f36973
VlIsLavead <143639454+VlIsLavead@users.noreply.github.com>
2025-05-07 14:49:37 +0300

    feat: add Profile edit success confirmation template (#74)
    Добавлена страница, подтверждающая успешное завершение редактирования профиля

--- account/views/__init__.py
+++ account/views/__init__.py
@@ -999,7 +999,7 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
-            messages.success(request, 'Profile updated successfully')
+            return render(request,'account/edit_profile_success.html')
         else:
             messages.error(request, 'Error updating your profile')
     else:

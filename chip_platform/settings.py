6cf41cd07303ac29b55fa7f9b1af51998ef11c30
VlIsLavead <143639454+VlIsLavead@users.noreply.github.com>
2025-05-28 18:47:37 +0300

    config: add vps specific settings (#92)
    Настройка для vps сервера

--- chip_platform/settings.py
+++ chip_platform/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = 'django-insecure-p+j4&4wmo2yl1s#2y@%lykfay%lfq=7&3r+==ves%7vwsi&l&w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.70', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['foundry-rf.ru', '192.168.1.70', 'localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition

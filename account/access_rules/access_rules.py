3b6ecca17503495a1ed9b87b0c2a7f2c50c167cf
VlIsLavead <143639454+VlIsLavead@users.noreply.github.com>
2025-07-29 18:44:57 +0300

    ref: extract access rules to a separate file, use constants (#141)
    Рефакторинг авторизации:
    - вынести правила доступа в отдельный файл
    - исполнитель: добавить правила доступа для просмотра страниц
    - добавить правила просмотра заказа

--- account/access_rules/access_rules.py
+++ account/access_rules/access_rules.py
@@ -1,4 +1,4 @@
-from account.models import Order
+from account.models import Order, Platform
 
 ACCESS_RULES = {
     Order.OrderStatus.NFW: ['Заказчик'],
@@ -19,3 +19,46 @@ ACCESS_RULES = {
     Order.OrderStatus.PS: ['Куратор'],
     Order.OrderStatus.CR: ['Заказчик'],
 }
+
+def check_view_permission(user_profile, order):
+    user_role = user_profile.role.name
+    user_company = user_profile.company_name
+    
+    if order.creator.user.id == user_profile.user.id:
+        return True
+    
+    if user_company == order.creator.company_name:
+        return True
+    
+    if user_role == 'Куратор':
+        return True
+    
+    if user_role == 'Исполнитель':
+        try:
+            user_platform = Platform.objects.get(platform_code=user_company)
+            if user_platform.id == order.platform_code_id:
+                return True
+        except Platform.DoesNotExist:
+            return False
+    
+    return False
+
+def check_edit_permission(user_profile, order):
+    """Проверка прав на редактирование заказа"""
+    user_role = user_profile.role.name
+    
+    if order.creator.user.id == user_profile.user.id:
+        return True
+    
+    if user_role == 'Куратор':
+        return True
+    
+    if user_role == 'Исполнитель':
+        try:
+            user_platform = Platform.objects.get(platform_code=user_profile.company_name)
+            if user_platform.id == order.platform_code_id:
+                return True
+        except Platform.DoesNotExist:
+            return False
+    
+    return False
--- account/decorators/restrict.py
+++ account/decorators/restrict.py
@@ -1,15 +1,15 @@
 from functools import wraps
 from django.shortcuts import render
-from account.models import Order
-from ..access_rules.access_rules import ACCESS_RULES
+from account.models import Order, Profile
+from ..access_rules.access_rules import ACCESS_RULES, check_view_permission, check_edit_permission
 
 def restrict_by_status(order_kwarg='order_id'):
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(request, *args, **kwargs):
             try:
-                user_role = request.user.profile.role.name
-            except Exception:
+                user_profile = Profile.objects.select_related('role').get(user=request.user)
+            except Profile.DoesNotExist:
                 return render(request, 'account/forbidden.html', {
                     'reason': 'У вашего пользователя отсутствует профиль или роль.'
                 }, status=403)
@@ -21,22 +21,27 @@ def restrict_by_status(order_kwarg='order_id'):
                 }, status=403)
 
             try:
-                order = Order.objects.get(id=order_id)
+                order = Order.objects.select_related('creator').get(id=order_id)
             except Order.DoesNotExist:
                 return render(request, 'account/forbidden.html', {
                     'reason': f'Заказ с номером {order_id} не найден.'
                 }, status=403)
-                
-            if (
-                order.creator.user_id != request.user.id
-                and request.user.profile.role.name not in ['Куратор', 'Исполнитель']
-            ):
+
+            # TODO отрефакторить, отвязаться от метаданных в коде
+            if view_func.__name__ == 'check_the_order':
+                if not check_view_permission(user_profile, order):
+                    return render(request, 'account/forbidden.html', {
+                        'reason': 'Вы не можете просматривать этот заказ.'
+                    }, status=403)
+                return view_func(request, *args, **kwargs)
+
+            if not check_edit_permission(user_profile, order):
                 return render(request, 'account/forbidden.html', {
                     'reason': 'Вы не можете редактировать этот заказ.'
                 }, status=403)
 
             allowed_roles = ACCESS_RULES.get(order.order_status, [])
-            if user_role not in allowed_roles:
+            if user_profile.role.name not in allowed_roles:
                 return render(request, 'account/forbidden.html', {
                     'reason': 'Вы не имеете доступ к этой странице.'
                 }, status=403)
